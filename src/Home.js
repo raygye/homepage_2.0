3cfdc6c7ff4b16370ed1cfa4b03f2b594773b516
yeG10 <garyye2007@gmail.com>
2020-07-27 00:54:34 -0400

    sidebar, start of intro

--- src/Home.js
+++ src/Home.js
@@ -3,9 +3,13 @@ import Sidebar from './Sidebar';
 export default class Home extends Component {
     render() {
         return (
-            <div className="row">
-                <Sidebar classname="col"></Sidebar>
-                <h1>Thanks for visiting!</h1>
+            <div className="row" style={{height: '100vh'}}>
+                <Sidebar classname="col" ></Sidebar>
+                <div className="col container mt-2">
+                    <h2>sorry, i'm bad with introductions</h2>
+                    <p>Thanks for visiting! My name is Gary Ye and I'm currently enrolled in 2A Computer Engineering at the University of Waterloo.
+                    </p>
+                </div>
             </div>
         )
     }
--- src/Sidebar.js
+++ src/Sidebar.js
@@ -4,23 +4,24 @@ import './Sidebar.css';
 export default class Sidebar extends Component {
     render() {
         return(
-            <nav className="navbar navbar-light" style={{width: '20%'}}>
-                <ul className="nav flex-column">
-                    <li className="nav-item">
-                        <Link className="nav-link active" to="/Home" replace style={{color: "black"}}><div className="txt">home</div></Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link className="nav-link" to="/About" replace style={{color: "black"}}><div className="txt">about</div></Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link className="nav-link" to="/Projects" replace style={{color: "black"}}><div className="txt">projects</div></Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link className="nav-link" to="/Experience" replace style={{color: "black"}}><div className="txt">experience</div></Link>
-                    </li>
-                </ul>
-            </nav>
-
+            <div className="container border-right" style={{width: '20%', height: '100%'}}>
+                <nav className="navbar navbar-light">
+                    <ul className="nav flex-column">
+                        <li className="nav-item">
+                            <Link className="nav-link active" to="/Home" replace style={{color: "black"}}><div className="txt">home</div></Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/About" replace style={{color: "black"}}><div className="txt">about</div></Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/Projects" replace style={{color: "black"}}><div className="txt">projects</div></Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/Experience" replace style={{color: "black"}}><div className="txt">experience</div></Link>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
         )
     }
 }
